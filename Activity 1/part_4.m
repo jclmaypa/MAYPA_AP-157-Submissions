094a62a4454b0674639b8deb1232dde536fcca03
jclmaypa <jlmaypa@up.edu.ph>
2023-03-01 15:47:30 +0800

    updates

--- Activity 1/part_4.m
+++ Activity 1/part_4.m
@@ -1,5 +1,19 @@
 Igray = rgb2gray(imread("dark_picture.png"));
-Imshow(Igray);
+imshow(Igray);
 
 %% 
 
+PDF = hist(Igray(:), [0:255])/numel(Igray);
+CDF = cumsum(PDF);
+figure; plot([0:255], CDF);
+
+%%
+x = [0:255];
+desiredCDF = (1/255) * x;
+newGS = interp1(desiredCDF, x, CDF(Igray(:)+1));
+Igraynew = reshape(newGS, size(Igray));
+figure; imshow(uint8(Igraynew));
+
+%%
+
+disp(CDF(Igray(:)+1));
